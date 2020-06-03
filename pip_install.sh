eb4bf5ad8890f4bcd28a8afad746fe17394c18fa
Muharrem Güner <64712867+heqmat@users.noreply.github.com>
2020-06-03 18:59:13 +0300

    Update pip_install.sh

--- pip_install.sh
+++ pip_install.sh
@@ -1,2 +1,5 @@
+# If you want install " pip " command to install modules. (If automatic pip command installer method is not successful.)
+# You can try this method.
+#---------------------------------------------------------------------------
 echo -e "\e[1;33m Checking pip versions..."
 sudo python get-pip.py
